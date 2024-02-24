379d29adf09b1ecf5b35a0fc657c5b83eb3b2c73
jinuklee <150532431+jinuk0211@users.noreply.github.com>
2024-02-25 00:08:25 +0900

    Update device.py

--- modules/device.py
+++ modules/device.py
@@ -77,7 +77,7 @@ def torch_gc(): #garbage collector
 def enable_tf32():
     if torch.cuda.is_available():
 
-        # enabling benchmark option seems to enable a range of cards to do fp16 when they otherwise can't
+        # tf32가 가능한 device의 경우 정밀도 조정 가능
         # see https://github.com/AUTOMATIC1111/stable-diffusion-webui/pull/4407
         device_id = (int(shared.cmd_opts.device_id) if shared.cmd_opts.device_id is not None and shared.cmd_opts.device_id.isdigit() else 0) or torch.cuda.current_device()
         if torch.cuda.get_device_capability(device_id) == (7, 5) and torch.cuda.get_device_name(device_id).startswith("NVIDIA GeForce GTX 16"):
@@ -117,7 +117,7 @@ def autocast(disable=False):
         return contextlib.nullcontext()
 
     if dtype == torch.float32 or shared.cmd_opts.precision == "full":
-        return contextlib.nullcontext()
+        return contextlib.nullcontext() #빈 컨텍스트를 제공
 
     return torch.autocast("cuda")
 
@@ -138,7 +138,7 @@ def test_for_nans(x, where):
         return
 
     if where == "unet":
-        message = "Unet에서 모든값이 None인 텐서 생성."
+        message = "Unet에서 NaN값 tensor 생성됨"
 
         if not shared.cmd_opts.no_half:
             message += "이미지 생성 과정 중 정확도 부족으로 인해 이미지가 정확하게 표현되지 않거나, 그래픽 카드가 특정 half type 데이터 형식을 지원하지 않아 계산 오류가 발생하는 경우 생길 수 있습니다. 정확도를 높이기 위해 특정 계산에 대해 더 정밀한 float32로 설정하세요 in Settings > Stable Diffusion 또는 -no-half commandline argument to fix this."
@@ -152,7 +152,7 @@ def test_for_nans(x, where):
         message = "A tensor with all NaNs was produced."
 
 
-    message += " Use --disable-nan-check commandline argument to disable this check."
+    message += " nan값 확인 기능을 끌려면 --disable-nan-check commandline argument를 command에 입력하세요."
 
     raise NansException(message)
 
